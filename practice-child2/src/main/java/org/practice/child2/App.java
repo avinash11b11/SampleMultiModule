2fb658d2cc84fb850f6c2783a4f3659d91652c87
Avinash <avinashbhat.avi@gmail.com>
2021-09-03 09:07:54 +0530

    First commit

---
+++ practice-child2/src/main/java/org/practice/child2/App.java
@@ -0,0 +1,13 @@
+package org.practice.child2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
